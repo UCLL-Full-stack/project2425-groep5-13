d3f098144e37a1e6cf803c68fc40fd2596a544b7
Janne Ackermans <janne.ackermans@student.ucll.be>
2024-10-15 12:21:20 +0200

    In 'reservatie.test.ts' import set form 'date-fns/set' to change 'timeSlot'

--- back-end/test/reservatie.test.ts
+++ back-end/test/reservatie.test.ts
@@ -1,10 +1,11 @@
+import set from "date-fns/set"
 import { Reservatie } from "../model/reservatie"
 import { TimeSlot } from "../model/timeSlot"
 
 test('given: valid values for reservatie; when: reservatie is created; then: reservatie is created with those values', () => {
     //given
     const id = 1
-    const timeSlot = new TimeSlot(new Date('2025-01-01T10:00:00Z'), new Date('2025-01-01T11:00:00Z'))
+    const timeSlot = new TimeSlot(set (new Date(), {year: 2025, month: 1, date:25, hours: 10}), set (new Date(), {year: 2025, month: 1, date:25, hours: 11}))
 
     //when
     const reservatie = new Reservatie({
