657b663633cef8c3ef721456a3ff2bdd2c7a3efc
LapinoLapidus <lapinolapidus@noreply.users.gitlab.com>
2024-12-19 12:32:48 +0100

    Previous commit++

--- back-end/repository/reservation.db.ts
+++ back-end/repository/reservation.db.ts
@@ -12,7 +12,7 @@ const getAllReservations = async (): Promise<Reservation[]> => {
         return reservationPrisma.map((reservationPrisma) => Reservation.from(reservationPrisma));
     } catch (error) {
         console.error(error);
-        throw new Error('Database error. See server log for details.');
+        throw new Error('Error getting all reservations.');
     }
 };
 
@@ -32,11 +32,49 @@ const getReservationsByUser = async (studentNumber: string): Promise<Reservation
         return reservationPrisma.map((reservationPrisma) => Reservation.from(reservationPrisma));
     } catch (error) {
         console.error(error);
-        throw new Error('Database error. See server log for details.');
+        throw new Error(`Error getting reservations by student number ${studentNumber}.`);
     }
 };
 
+const getReservation = async (reservationId: number): Promise<Reservation> => {
+    try {
+        console.log(reservationId);
+        const reservationPrisma = await database.reservation.findMany({
+            where: {
+                id: reservationId,
+            },
+            include: {
+                classroom: true,
+                user: true
+            }
+        });
+        console.log(reservationPrisma);
+        if (!reservationPrisma || reservationPrisma.length === 0) {
+            return null;
+        }
+        return Reservation.from(reservationPrisma[0]);
+    } catch (error) {
+        console.error(error);
+        throw new Error(`Error getting reservation with id ${reservationId}.`);
+    }
+}
+
+const deleteReservation = async (reservationId: number): Promise<void> => {
+    try {
+        await database.reservation.delete({
+            where: {
+                id: reservationId,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        throw new Error(`Error deleting reservation with id ${reservationId}.`);
+    }
+}
+
 export default {
     getAllReservations,
-    getReservationsByUser
+    getReservationsByUser,
+    getReservation,
+    deleteReservation
 };
--- back-end/service/reservation.service.ts
+++ back-end/service/reservation.service.ts
@@ -7,7 +7,25 @@ const getReservationsByUser = async (studentNumber: string): Promise<Reservation
     return reservationDb.getReservationsByUser(studentNumber);
 };
 
+const getReservation = async (reservationId: number): Promise<Reservation> => {
+    try {
+        return reservationDb.getReservation(reservationId);
+    } catch (e) {
+        throw e;
+    }
+};
+
+const deleteReservation = async (reservationId: number): Promise<void> => {
+    try {
+        await reservationDb.deleteReservation(reservationId);
+    } catch (e) {
+        throw e;
+    }
+}
+
 export default {
     getAllReservations,
     getReservationsByUser,
+    getReservation,
+    deleteReservation
 };
